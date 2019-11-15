0023f4d18989724e7cf8b58c818e48f1e63ded3c
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-15 11:27:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,7 @@
  require 'nokogiri'
  require 'pry'
 
-#
+#We click around a bit in the Chrome web inspector, take a stab at a CSS selector in Pry, and then keep track of that selector in our project file. 
 
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
